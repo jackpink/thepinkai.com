e06693def80af058c90d4f4591edc65bcf7bd9ea
Jack Pinkerton <jack@fidelitysystems.com.au>
2023-11-12 17:46:15 +1100

    Homepage just with logo for now, need to add an ad + gif

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,19 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const techBlogPosts = await getCollection('blog');
+	const supercoachBlogPosts = await getCollection('supercoach-blog');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: techBlogPosts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
+		})),
+		supercoach: supercoachBlogPosts.map((post) => ({
+			...post.data,
+			link: `/supercoach-blog/${post.slug}/`,
 		})),
 	});
 }
