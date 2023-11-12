e0c666716a3002138883d0672aa8a34e5b33ef01
Jack-Desktop <jackpinkerton@live.com.au>
2023-11-12 14:09:12 +1100

    two blog directories

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,23 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
+const blogSchema = z.object({
+	title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
-	}),
+	});
+
+const techBlogCollection = defineCollection({
+	// Type-check frontmatter using a schema
+	type: 'content',
+	schema: blogSchema,
 });
 
-export const collections = { blog };
+const supercoachBlogCollection = defineCollection({
+	type: 'content',
+	schema: blogSchema,
+});
+
+export const collections = { 'blog': techBlogCollection, 'supercoach-blog': supercoachBlogCollection };
