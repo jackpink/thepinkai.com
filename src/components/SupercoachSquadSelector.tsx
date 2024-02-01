61847a621b2d61dfcec5d270d854c62a9882f1ea
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-02 08:42:34 +1100

    Merged main with react import

---
+++ src/components/SupercoachSquadSelector.tsx
@@ -0,0 +1,12 @@
+
+
+const SupercoachSquadSelector = () => {
+
+    return(
+        <div>
+            <h1>Supercoach Squad Selector</h1>
+        </div>
+    )
+}
+
+export default SupercoachSquadSelector;
