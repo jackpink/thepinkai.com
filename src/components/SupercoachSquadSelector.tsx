ef1627545144a8837bdd0c71d7b9eb07fc8c1818
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-02 09:48:54 +1100

    react api component cerated

--- src/components/SupercoachSquadSelector.tsx
+++ src/components/SupercoachSquadSelector.tsx
@@ -1,12 +1,37 @@
+import {QueryClient, QueryClientProvider, useQuery, useQueryClient} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const SupercoachSquadSelectorApp = () => {
 
-const SupercoachSquadSelector = () => {
+    const queryClient = new QueryClient();
 
     return(
-        <div>
-            <h1>Supercoach Squad Selector</h1>
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <SupercoachSquadSelector/>
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
     )
 }
 
-export default SupercoachSquadSelector;
+export default SupercoachSquadSelectorApp;
+
+const boredApiQuery = async () => {
+    const response = await fetch('https://www.boredapi.com/api/activity');
+    return response.json();
+}
+
+const SupercoachSquadSelector = () => {
+
+    const queryClient = useQueryClient();
+
+    const {data} = useQuery({ queryKey: ['boredApi'], queryFn: boredApiQuery})
+    console.log(data);
+
+    return(
+        <div>
+        <h1>New Supercoach Squad Selector with API</h1>
+        {data ? <p>{data.activity}</p> : <p>Loading...</p>}
+        <button onClick={() => queryClient.invalidateQueries({queryKey: ['boredApi']})}>click me</button>
+</div>
+    )
+}
