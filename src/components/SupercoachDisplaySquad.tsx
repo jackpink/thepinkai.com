1c643938057229c1c2ea815c3f0dad0453317e83
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-05 16:29:40 +1100

    built shell for player display

---
+++ src/components/SupercoachDisplaySquad.tsx
@@ -0,0 +1,155 @@
+import { type Player, type Squad } from "./SupercoachSquadSelector"
+
+const PlayerInfo = ({team, price}: {team: string, price: number}) => {
+    const OuterDivStyle = {
+        display: 'inline-block',
+        paddingLeft: '10px'
+    }
+    const TextStyle = {
+        margin: '0px',
+        fontFamily: 'Arial'
+    }
+
+    const priceString = price.toString()
+
+    return(<div style={OuterDivStyle}>
+<p style={TextStyle}>{team}</p>
+<p style={TextStyle}>{'$' + priceString}</p>
+</div>)
+}
+
+const TeamColours = ({team}: {team: string}) => {
+    let colours = ['white', 'white', 'white']
+    switch (team) {
+    case 'BUL':
+        colours = ['blue', 'white', 'blue']
+        break;
+    case 'CBR':
+        colours = ['green', 'white', 'yellow']
+        break;
+    case 'SHA':
+        colours = ['blue', 'white', 'black']
+        break;
+    case 'MEL':
+        colours = ['purple', 'white', 'purple']
+        break;
+    case 'NQL':
+        colours = ['blue', 'yellow', 'blue']
+        break;
+    case 'GCT':
+        colours = ['lightblue', 'white', 'lightblue']
+        break
+    case 'MNL':
+        colours = ['red', 'white', 'red']
+        break;
+    case 'PEN':
+        colours = ['black', 'white', 'black']
+        break;
+    case 'PAR':
+        colours = ['blue', 'gold', 'blue']
+        break;
+    case 'NEW':
+        colours = ['blue', 'red', 'blue']
+        break;
+    case 'NZW':
+        colours = ['black', 'white', 'black']
+        break;
+    case 'STI':
+        colours = ['red', 'white', 'red']
+        break;
+    case 'SYD':
+        colours = ['red', 'white', 'red']
+        break;
+    case 'WST':
+        colours = ['orange', 'black', 'orange']
+        break
+    case 'DOL':
+        colours = ['purple', 'white', 'purple']
+        break;
+    }
+
+    const FlexBoxStyle = {
+        display: 'flex'
+    }
+    const TeamColourLeftStyle = {
+        width: '20px',
+        height: 'auto',
+        backgroundColor: colours[0],
+        display: 'inline-block'
+    }
+    const TeamColourMidStyle = {
+        width: '20px',
+        height: 'auto',
+        backgroundColor: colours[1],
+        display: 'inline-block'
+    }
+    const TeamColourRightStyle = {
+        width: '20px',
+        height: 'auto',
+        backgroundColor: colours[2],
+        display: 'inline-block'
+    }
+
+    return(<div style={FlexBoxStyle}>
+
+<div style={TeamColourLeftStyle}></div>
+<div style={TeamColourMidStyle}></div>
+<div style={TeamColourRightStyle}></div>
+</div>)
+}
+
+const Player = ({player}: {player: Player}) => {
+    const OuterDivStyle = {
+        border: '1px solid black',
+        borderRadius: '8px',
+        width: '180px',
+        overflow: 'hidden',
+        display: 'inline-block'
+    }
+    const PlayerNameDivStyle = {
+        backgroundColor: '#494B4C',
+        margin: '0px',
+        padding: '5px'
+    }
+    const PlayerNameTextStyle = {
+        margin: '0px',
+        color: 'white',
+        fontFamily: 'Arial'
+    }
+    const PlayerInfoDivStyle = {
+        padding: '10px',
+        backgroundColor: '#C8C9C9'
+    }
+    return(
+        <div style={OuterDivStyle}>
+            <div style={PlayerNameDivStyle}>
+                <p style={PlayerNameTextStyle}>{player.Name}</p>
+            </div>
+            <div style={PlayerInfoDivStyle}>
+                <TeamColours team={player.Team}/>
+                <PlayerInfo team={player.Team} price={player.Price}/>
+            </div>
+        </div>
+    )
+}
+
+ export const DisplaySquad = ({squad}: {squad: Squad}) => {
+    return(
+        <div>
+            <h1>Fullbacks</h1>
+            {squad.Fullbacks.map((player, index) => <Player key={index} player={player}/>)}
+            <h1>CentreWingers</h1>
+            {squad.CentreWingers.map((player, index) => <Player key={index} player={player}/>)}
+            <h1>FiveEights</h1>
+            {squad.FiveEights.map((player, index) => <Player key={index} player={player}/>)}
+            <h1>Halfbacks</h1>
+            {squad.Halfbacks.map((player, index) => <Player key={index} player={player}/>)}
+            <h1>FrontRowers</h1>
+            {squad.FrontRowers.map((player, index) => <Player key={index} player={player}/>)}
+            <h1>SecondRowers</h1>
+            {squad.SecondRowers.map((player, index) => <Player key={index} player={player}/>)}
+            <h1>Hookers</h1>
+            {squad.Hookers.map((player, index) => <Player key={index} player={player}/>)}
+        </div>
+    )
+}
--- src/components/SupercoachSquadSelector.tsx
+++ src/components/SupercoachSquadSelector.tsx
@@ -1,5 +1,55 @@
 import {QueryClient, QueryClientProvider, useQuery, useQueryClient} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { DisplaySquad } from "./SupercoachDisplaySquad";
+
+export type Squad = {
+    Fullbacks: Player[];
+    CentreWingers: Player[];
+    FiveEights: Player[];
+    Halfbacks: Player[];
+    FrontRowers: Player[];
+    SecondRowers: Player[];
+    Hookers: Player[];
+}
+
+export type Player = {
+    Name: string;
+    Position: string;
+    Price: number;
+    Team: string;
+}
+
+function isPlayer(player: any): player is Player {
+    return (
+      typeof player === "object" &&
+      player !== null &&
+      "Name" in player &&
+      
+      "Price" in player &&
+        "Team" in player
+    );
+  }
+
+const isSquad = (squad: any): squad is Squad => {
+    return (
+        typeof squad === "object" &&
+        squad !== null &&
+        "Fullbacks" in squad &&
+        squad["Fullbacks"].every(isPlayer) &&
+        "CentreWingers" in squad &&
+        squad["CentreWingers"].every(isPlayer) &&
+        "FiveEights" in squad &&
+        squad["FiveEights"].every(isPlayer) &&
+        "Halfbacks" in squad &&
+        squad["Halfbacks"].every(isPlayer) &&
+        "FrontRowers" in squad &&
+        squad["FrontRowers"].every(isPlayer) &&
+        "SecondRowers" in squad &&
+        squad["SecondRowers"].every(isPlayer) &&
+        "Hookers" in squad &&
+        squad["Hookers"].every(isPlayer)
+    );
+}
 
 const SupercoachSquadSelectorApp = () => {
 
@@ -17,20 +67,24 @@ export default SupercoachSquadSelectorApp;
 
 const boredApiQuery = async () => {
     const response = await fetch('https://nrl-supercoach-ai-46zughvc3a-ts.a.run.app/');
-    return response.json();
+    const squad = await response.json();
+    if (isSquad(squad)) {
+        return squad
+    }
+    throw new Error("Invalid response");
 }
 
 const SupercoachSquadSelector = () => {
 
     const queryClient = useQueryClient();
 
-    const {data, error} = useQuery({ queryKey: ['boredApi'], queryFn: boredApiQuery})
-    console.log(data);
+    const {data: squad, error} = useQuery({ queryKey: ['boredApi'], queryFn: boredApiQuery})
+    // console.log(squad);
 
     return(
         <div>
         <h1>New Supercoach Squad Selector with API</h1>
-        {data && data["Fullbacks"] && data["Fullbacks"][0] ? <p>{data["Fullbacks"][0].Name}</p> : <p>Loading...</p>}
+        {squad? <DisplaySquad squad={squad} />: <p>Loading...</p>}
         {error && <p>Error: {error.message}</p>}
         <button onClick={() => queryClient.invalidateQueries({queryKey: ['boredApi']})}>click me</button>
 </div>
